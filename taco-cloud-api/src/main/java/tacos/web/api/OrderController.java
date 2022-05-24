4ed54a68d9598ed56347f5a8ee94914b712dc21a
Sao Viet <Sao Viet@Hieu-Laptop>
2022-05-24 08:47:30 +0700

    up

--- taco-cloud-api/src/main/java/tacos/web/api/OrderController.java
+++ taco-cloud-api/src/main/java/tacos/web/api/OrderController.java
@@ -1,10 +1,15 @@
 package tacos.web.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import tacos.Order;
 import tacos.data.OrderRepository;
 
 @RestController
@@ -16,6 +21,12 @@ public class OrderController {
 	@Autowired
 	public OrderController(OrderRepository orderRepo) {
 		this.orderRepo = orderRepo;
+	}
+	
+	@PostMapping(consumes = "application/json")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Order postOrder(@RequestBody Order order) {
+		return orderRepo.save(order);
 	}
 	
 }
