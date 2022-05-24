44118dd584a4101345a0e336b1decb489c8b98ce
Sao Viet <Sao Viet@Hieu-Laptop>
2022-05-24 09:54:16 +0700

    up

--- taco-cloud-client/src/main/java/tacos/web/OrderController.java
+++ taco-cloud-client/src/main/java/tacos/web/OrderController.java
@@ -6,14 +6,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
 import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
+import tacos.Taco;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
-
+	private RestTemplate rest = new RestTemplate();
 
 	@GetMapping("/current")
 	public String orderForm(Model model) {
@@ -23,8 +27,8 @@ public class OrderController {
 
 	@PostMapping
 	public String processOrder(Order order) {
+		rest.postForObject("http://localhost:8080/design", order, Order.class);
 		log.info("Order submitted: " + order);
-		return "redirect:/";
-
+		return "redirect:/home";
 	}
 }
